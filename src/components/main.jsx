84fd22da139a071158ebef5e9b88a05f59d25a85
Guy <ezraguy1@gmail.com>
2022-09-07 21:13:19 +0300

    first commit

---
+++ src/components/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../scss/main.scss';
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="title">Hello world!</div>
+    </div>
+  );
+};
+
+export default Main;
